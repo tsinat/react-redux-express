1cfc40aa8a4ef263e5074bb39e913cc04bfdbd04
Tsinat Zeree <cheru87@gmail.com>
2016-09-28 01:30:24 -0700

    webpack configuration

--- client/index.js
+++ client/index.js
@@ -3,5 +3,5 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 render(
-    <App /> , document.getElementById('app');
-)
+    <App /> , document.getElementById('app')
+);
