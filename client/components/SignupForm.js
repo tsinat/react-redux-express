7db949de8470a3a1118e313478993795b88cac40
Tsinat Zeree <cheru87@gmail.com>
2016-09-28 14:57:20 -0700

    complete signup form

---
+++ client/components/SignupForm.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Component } from 'react';
+import timezones from '../data/timezones';
+import map from 'lodash/map';
+
+class SignupForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+            timeZone: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log('this.state:', this.state);
+    }
+    render() {
+        const options = map(timezones, (val, key) =>
+            <option key={val} value={val}>{key}</option>
+        );
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Join our community!</h1>
+                <div className='from-group'>
+                    <label className='control-label'>UserName:</label>
+                    <input
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        type='text'
+                        name='username'
+                        className='form-control'
+                    />
+                </div>
+                <div className='from-group'>
+                    <label className='control-label'>Email:</label>
+                    <input
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        type='email'
+                        name='email'
+                        className='form-control'
+                    />
+                </div>
+                <div className='from-group'>
+                    <label className='control-label'>Password:</label>
+                    <input
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type='password'
+                        name='password'
+                        className='form-control'
+                    />
+                </div>
+                <div className='from-group'>
+                    <label className='control-label'>Password Confirmation:</label>
+                    <input
+                        value={this.state.passwordConfirmation}
+                        onChange={this.handleChange}
+                        type='password'
+                        name='passwordConfirmation'
+                        className='form-control'
+                    />
+                </div>
+                <div className='from-group'>
+                    <label className='control-label'>Timezone:</label>
+                    <select
+                        className='form-control'
+                        name='timezone'
+                        value={this.state.timezone}
+                        onChange={this.handleChange}
+                    >
+                    <option value='' disabled>Choose Your Timezone</option>
+                    {options}
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-primary btn-lg'>
+                        Sign up
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default SignupForm;
--- client/components/SignupPage.js
+++ client/components/SignupPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SignupForm from './SignupForm';
+
 class SignupPage extends Component {
     render() {
         return (
-            <h1>Sign up Page</h1>
+            <div className='row'>
+                <div className='col-md-4 col-md-offset-4'>
+                    <SignupForm />
+                </div>
+            </div>
         );
     }
 }
