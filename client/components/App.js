69dfd595708da9852baa87e7a19ada9ef4a22e7e
Tsinat Zeree <cheru87@gmail.com>
2016-10-03 23:33:51 -0700

    adding and removing flash messaged

--- client/components/App.js
+++ client/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Component } from 'react';
 import Greetings from './Greetings';
 import NavigationBar from './NavigationBar';
+import flashMessagesList from './flash/flashMessagesList';
 
 class App extends Component {
     render(){
         return (
             <div className='container'>
                 <NavigationBar />
+                <flashMessagesList />
                 {this.props.children}
             </div>
         )
